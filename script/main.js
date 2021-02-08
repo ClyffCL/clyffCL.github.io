9eb43feac3d09909214d0f670adf612855aa77e5
ClyffCL <toalejandrovillagran@gmail.com>
2021-02-08 02:14:16 -0300

    linux version

--- script/main.js
+++ script/main.js
@@ -2,8 +2,8 @@ let miImage = document.querySelector('img');
 miImage.onclick = function () {
     let miSrc = miImage.getAttribute('src');
     if (miSrc === 'images/culo2.jpg') {
-      miImage.setAttribute('src','images/culo3');
+      miImage.setAttribute('src','images/culo3.jpg');
     } else {
-      miImage.setAttribute('src', 'images/culo2');
+      miImage.setAttribute('src', 'images/culo2.jpg');
     }
 }
