5ee73ca62e52c9b4770daa1a5c67a3a4eb5db96f
ClyffCL <toalejandrovillagran@gmail.com>
2021-02-08 02:09:00 -0300

    modificado en linux

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
-páksodjasjdaoskdoasnasca
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/culo2.jpg') {
+      miImage.setAttribute('src','images/culo3');
+    } else {
+      miImage.setAttribute('src', 'images/culo2');
+    }
+}
