14934009858846076142811e4d248c79ef0a6996
ClyffCL <toalejandrovillagran@gmail.com>
2021-02-10 23:41:25 -0300

    modificado desde linux

--- script/main.js
+++ script/main.js
@@ -1,9 +1,22 @@
-let miImage = document.querySelector('img');
-miImage.onclick = function () {
-    let miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/culo2.jpg') {
-      miImage.setAttribute('src', 'images/culo3.jpg');
-    } else {
-      miImage.setAttribute('src', 'images/culo2.jpg');
-    }
+
+const list = document.createElement('ol');
+const info = document.createElement('p');
+const p = document.querySelector('p');
+
+info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+
+document.body.appendChild(info);
+document.body.appendChild(list);
+
+p.onclick = function() {
+  const listItem = document.createElement('li');
+  const listContent = prompt('What content do you want the list item to have?');
+  listItem.textContent = listContent;
+  list.appendChild(listItem);
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
 }
